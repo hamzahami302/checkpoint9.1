c4ca04c876fd7dfa5fccab513076a48604f995b0
hamza hami <hamihamza423@gmail.com>
2019-09-03 16:07:15 +0100

    cmmit 9.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Navbar, Nav} from 'react-bootstrap'
 import './App.css';
-
+import Acceuil from './Acceuil';
+import Competence from './competence';
+import Formation from './formation';
+import {BrowserRouter , Link, Route,Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <BrowserRouter>
+
+     <Navbar bg="light" expand="lg">
+   <Navbar.Brand href="#home">React-js</Navbar.Brand>
+   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+   <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+     <div className="row col-12">
+     <Nav className="col-12 ml-auto">
+      <Nav.Link href="#acceuil">Acceuil</Nav.Link>
+      <Nav.Link href="#competence">Competence</Nav.Link>
+      <Nav.Link href="#formation">Formation</Nav.Link>
+      </Nav></div>
+      </Navbar>
+      
+      
+
+      
+    
+  
+     <Switch>
+       
+       <Route exact path="/" component={Acceuil}></Route>
+       <Route path="/competence" component={Competence}></Route>
+       <Route path="/formation" component={Formation}></Route>
+     </Switch>
+
+ 
+     </BrowserRouter>
+
+    
+
     </div>
   );
 }
